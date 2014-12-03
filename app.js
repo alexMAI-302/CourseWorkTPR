eec08c5d3adbdaa0816b81974dea63831c36d4b4
alexMAI-302 <alex71321@yandex.ru>
2014-12-03 15:50:01 +0400

    Publ5
    Publ5

--- app.js
+++ app.js
@@ -1,53 +1,48 @@
 Ext.require('Ext.slider.*');
 
-var criterion = ["Price", "Color", "Speed"];
-var criterionValue = new Array(criterion.length);
+var criterion = ["Price", "Color", "Speed", "Engine", "Двери"];
 
-var a = 0.5, b = 0.5, c = 0.5;
+var SlidersCount;
+if(criterion.length == 2)
+    SlidersCount = 1;
+if((criterion.length%2) != 0)
+    SlidersCount = criterion.length*(Math.floor(criterion.length/2));
+else
+    SlidersCount = (criterion.length-1)+(criterion.length-1)*(Math.floor((criterion.length-1)/2));
+
+
+var criterionSliders = new Array(SlidersCount);
+k=0;
+for(i=0; i<criterion.length; i++)
+    for(j=i+1; j<criterion.length; j++)
+    {
+        criterionSliders[k] = criterion[i]+criterion[j];
+        k++;
+    }
+
+var criterionValue = new Array(SlidersCount);
+    for(i = 0; i < SlidersCount; i++)
+        criterionValue[i] = 0.5;
+
 
 Ext.onReady(function(){
+	for(i = 0; i < SlidersCount; i++)
+    {
 
-    Ext.create('Ext.slider.Single', {
-        renderTo: 'custom-tip-slider1',
-        hideLabel: true,
-        width: 260,
-        increment: 1,
-        minValue: 0,
-        maxValue: 100,
-		value: 50,
-        tipText: function(thumb){
-			criterionValue[0] = thumb.value/100;
-			return Ext.String.format('<b>{0}% важность</b>', thumb.value);
-        }
-		
-    });
-	
-	Ext.create('Ext.slider.Single', {
-        renderTo: 'custom-tip-slider2',
-        hideLabel: true,
-        width: 267,
-        increment: 1,
-        minValue: 0,
-        maxValue: 100,
-		value: 50,
-        tipText: function(thumb){
-			criterionValue[1] = thumb.value/100;
-            return Ext.String.format('<b>{0}% важность</b>', thumb.value);
-        }
-    });
-	
-	Ext.create('Ext.slider.Single', {
-        renderTo: 'custom-tip-slider3',
-        hideLabel: true,
-        width: 275,
-        increment: 1,
-        minValue: 0,
-        maxValue: 100,
-		value: 50,
-        tipText: function(thumb){
-			criterionValue[2] = thumb.value/100;
-            return Ext.String.format('<b>{0}% важность</b>', thumb.value);
-        }
-    });
-   
+       Ext.create('Ext.slider.Single', {
+            renderTo: Ext.getBody(), 
+            hideLabel: true,
+            clickToChange: true,
+            width: 260,
+            increment: 1,
+            minValue: 0,
+            maxValue: 100,
+            value: 50,
+            id: criterionSliders[i] + 'Slider',
+            tipText: function(thumb){
+                return Ext.String.format('<b>{0}% важность</b>', thumb.value);
+            }
+
+        });
+    }
 });
