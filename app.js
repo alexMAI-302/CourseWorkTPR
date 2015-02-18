542e80a641a6f43909c6e41e0b93acb3015b0dac
alexMAI-302 <Alex71321@yandex.ru>
2015-02-18 22:52:12 +0400

    Revert "LastPubl"
    This reverts commit 23703cb8b765717fb566e8bb4b7ae5e7d85b0cb7.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 Ext.require('Ext.slider.*');
-var criterion = ["Price", "Color", "Speed", "Engine"];
+var criterion = ["Price", "Color", "Speed", "Engine", "Двери"];
 
 var SlidersCount;
 if(criterion.length == 2)
@@ -10,14 +10,12 @@ else
     SlidersCount = (criterion.length-1)+(criterion.length-1)*(Math.floor((criterion.length-1)/2));
 
 
-var LCriterion = new Array(SlidersCount);
-var RCriterion = new Array(SlidersCount);
+var criterionSliders = new Array(SlidersCount);
 k=0;
 for(i=0; i<criterion.length; i++)
     for(j=i+1; j<criterion.length; j++)
     {
-        LCriterion[k] = criterion[i];
-        RCriterion[k] = criterion[j];
+        criterionSliders[k] = criterion[i]+criterion[j];
         k++;
     }
 
@@ -28,29 +26,20 @@ var criterionValue = new Array(SlidersCount);
 Ext.onReady(function(){
     for(i = 0; i < SlidersCount; i++)
     {
-        Ext.create('Ext.panel.Panel', {
-            title: 'Установите соотношение №' + (i+1) + ':',
-            width: 350,
-            height: 70,
-            lbar: [LCriterion[i]],
-            rbar: [RCriterion[i]],
-            id : 'TitlePanel' + i,
-            renderTo: Ext.getBody()
-        });
         Ext.create('Ext.slider.Single', {
-            renderTo: Ext.getBody(), 
-            hideLabel: false,
-            fieldLabel: '',
-            clickToChange: true,
-            width: 350,
-            increment: 1,
-            minValue: 0,
-            maxValue: 100,
-            value: 50,
-            id: LCriterion[i] + RCriterion[i] + 'Slider',
-            tipText: function(thumb){
-                return Ext.String.format('<b>{0}% важность</b>', thumb.value);
-            }        
+                renderTo: Ext.getBody(), 
+                hideLabel: false,
+                fieldLabel: 'Установите соотношение '+ criterionSliders[i],
+                clickToChange: true,
+                width: 350,
+                increment: 1,
+                minValue: 0,
+                maxValue: 100,
+                value: 50,
+                id: criterionSliders[i] + 'Slider',
+                tipText: function(thumb){
+                    return Ext.String.format('<b>{0}% важность</b>', thumb.value);
+                }
         });
     }
 });
